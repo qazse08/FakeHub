058b2dc4df08c8a429f562ed2d6365d9ac6f2fc8
qazse08 <150704995+qazse08@users.noreply.github.com>
2026-05-21 14:48:26 +0700

    Update FakeHub.lua

--- FakeHub.lua
+++ FakeHub.lua
@@ -1,1 +1,4 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/TITANIC-HUB/THUB/main/Loader.lua"))()
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/TITANIC-HUB/THUB/main/Loader.lua"))()
+
+script_key="xBxjnlLcrNxEbkcTTeHighMlQiCSJhcn";
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/1dcf88ec9da09b6f22014bd23ef879b4.lua"))()
